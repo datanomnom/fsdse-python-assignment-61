68b4764f4dcb40a9781c6ce64de307ed1d274e13
datanomnom <ronzzz92@gmail.com>
2017-06-18 08:27:42 +0000

    Done

--- build.py
+++ build.py
@@ -1,2 +1,9 @@
 def solution(list_of_nums):
     """Enter Code Here"""
+    result = {'ODD': 0, 'EVEN': 0}
+    for num in list_of_nums:
+        if num % 2 == 0:
+            result['EVEN']+=1
+        else:
+            result['ODD']+=1
+    return result
